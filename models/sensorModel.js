4def7e0915dafa8002c368d537c5624b9ac1be80
Kevin Bruce Mulinda <89967620+Dukent29@users.noreply.github.com>
2025-05-20 18:16:11 +0200

    update sesorModel.js

---
+++ models/sensorModel.js
@@ -0,0 +1,11 @@
+const db = require('../config/pg');
+
+const SensorData = {
+    getByPlantId: async (plant_id) => {
+        const query = 'SELECT * FROM sensor_data WHERE plant_id = $1 ORDER BY timestamp DESC';
+        const result = await db.query(query, [plant_id]);
+        return result.rows;
+    }
+};
+
+module.exports = SensorData;
