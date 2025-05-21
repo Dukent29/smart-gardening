82ae52e0e065b9e5b137b7f57233938754dd2131
Kevin Bruce Mulinda <89967620+Dukent29@users.noreply.github.com>
2025-05-21 15:48:57 +0200

    updated actionModel

--- models/actionModel.js
+++ models/actionModel.js
@@ -1,8 +1,8 @@
-const db = require('../config/db');
+const db = require('../config/pg');
 
 const PlantAction = {
     getByPlantId: async(plant_id) => {
-            db.query ='SELECT * FROM actions WHERE plant_id = $1 ORDER BY timestamp DESC';
+            const query ='SELECT * FROM actions WHERE plant_id = $1 ORDER BY timestamp DESC';
             const results = await db.query(query, [plant_id]);
             return results.rows;
         }
