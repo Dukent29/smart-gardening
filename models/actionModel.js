332ef04866499eb037db2b5c3026939321b89754
Kevin Bruce Mulinda <89967620+Dukent29@users.noreply.github.com>
2025-05-21 19:44:40 +0200

    updated actionModel, sensorController, sensors,

--- models/actionModel.js
+++ models/actionModel.js
@@ -5,7 +5,16 @@ const PlantAction = {
             const query ='SELECT * FROM actions WHERE plant_id = $1 ORDER BY timestamp DESC';
             const results = await db.query(query, [plant_id]);
             return results.rows;
-        }
+        },
+create: async ({ plant_id, action, action_type, timestamp }) => {
+    const result = await db.query(
+        'INSERT INTO actions (plant_id, action_description, action_type, timestamp) VALUES ($1, $2, $3, $4)',
+        [plant_id, action, action_type, timestamp]
+    );
+    return result;
+},
+
     };
+
 
     module.exports = PlantAction;
